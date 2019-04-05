2164ff158b9a0237d244f4f5c9752f3cb58073e9
lrnzgiusti <lrnz.justy@gmail.com>
2019-04-05 14:55:25 +0200

    First Commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  src
+//
+//  Created by Lorenzo Giusti on 05/04/2019.
+//  Copyright © 2019 Lorenzo Giusti. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello Motif-Counter\n";
+    return 0;
+    
+}
